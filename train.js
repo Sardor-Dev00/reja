58768ac49528005d70e4985bfa9d23818c1ba8d8
Sardor <eshonkulovsarik14@gmail.com>
2025-02-23 18:50:29 +0900

    feat:callback function learned

--- train.js
+++ train.js
@@ -1,1 +1,64 @@
-console.log("Train ishga tushdi");
+console.log("Jack Ma maslahatlari");
+const list = [
+  "yahshi talaba boling" ,// 0-20
+  "togri boshliq tanlang va koproq hato qiling", // 20-30
+  "uzingizga ishlashni boshlang", // 30-40
+  "siz kuchli bolgan narsalarni qiling", // 40-50
+  "yoshlarga investitsiya qiling", // 50-60
+  "endi dam oling, foydaysi yoq endi" // 60
+];
+
+function maslahatBering(a, callback) {
+  if(typeof a !== "number") callback("insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setTimeout(() => {
+      callback(null, list[5]);
+    }, 5000);
+  }
+}
+
+// maslahatBering(10, (err, data) => {
+//   if(err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+
+// maslahatBering(25, (err, data) => {
+//   if(err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+
+// maslahatBering(55, (err, data) => {
+//   if(err) console.log("ERROR:", err);
+//   console.log("javob:", data);
+// });
+
+// maslahatBering("salom!", (err, data) => {
+//   if(err) console.log("ERROR:", err);
+//   else {
+//     console.log("javob:", data);
+//   }
+// });
+
+// console.log("passed here 1");
+// maslahatBering(65, (err, data) => {
+//   if(err) console.log("ERROR:", err);
+//   else {
+//     console.log("javob:", data);
+//   }
+// });
+// console.log("passed here 2");
+
+console.log("passed here 1");
+maslahatBering(65, (err, data) => {
+  if(err) console.log("ERROR:", err);
+  else {
+    console.log("javob:", data);
+  }
+});
+console.log("passed here 2");
+
